883b7f08a6818ec1b83de981bfc65e3d5c0f9efb
Stela Capsa <stela.capsa@gmail.com>
2020-02-17 16:23:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Mr. Chocolate", 10, "M", "Brown", "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Rowdy", 4, "M", "Black", "Agresive", 1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Rowdy", 4, "F", "Black", "Agresive", 1);
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Tabitha", 8, "F", "White", "Wild", 0);
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Sergeant Brown", 4, "M", "Brown", "Calm", 1);
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Melissa", 9, "F", "White", "Calm", 0);
