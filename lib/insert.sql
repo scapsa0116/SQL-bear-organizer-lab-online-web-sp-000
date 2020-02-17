91b0b16600eafbe0c2311e3dcd2c4c1e72b8f116
Stela Capsa <stela.capsa@gmail.com>
2020-02-17 16:23:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Mr. Chocolate", 10, "Male", "Brown", "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Rowdy", 4, "Male", "Black", "Agresive", 1);
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Tabitha", 8, "Female", "White", "Wild", 0);
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Sergeant Brown", 4, "Male", "Brown", "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Melissa", 9, "Female", "White", "Calm", 0);
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Grinch", 8, "Male", "Black", "Wild", 1);
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Wendy", 4, "Female", "Black", "Agresive", 1);
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES (NULL, "Male", "Black", "Agresive", 1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Mr. Chocolate", 10, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Rowdy", 4, "M", "Black", "Agresive", 1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Tabitha", 8, "F", "White", "Wild", 0);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Sergeant Brown", 4, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Melissa", 9, "F", "White", "Calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Grinch", 8, "M", "Black", "Wild", 1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Wendy", 4, "F", "Black", "Agresive", 1);
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES (NULL, 5, "M", "Black", "Agresive", 1);
